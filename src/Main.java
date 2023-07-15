3b5168513b8513619187a115c7a038c123a809de
kyupid <kyeonu1007@gmail.com>
2023-07-15 11:24:34 +0900

    Tuple: 자바 버전
    - 세 개의 요소를 그룹화한 튜플은 어떻게?
    - 튜플을 그룹화하려면?

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Pair<String, String> raoul = new Pair<>("Raoul", "+44 007007007");
+        Pair<String, String> alan = new Pair<>("Alan", "+44 0034134839");
+    }
+}

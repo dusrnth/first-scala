45ed8d402818c8fee775a6f450181aa9e14a05e2
kyupid <kyeonu1007@gmail.com>
2023-07-15 11:06:46 +0900

    Collection: List, Set
    - 모두 불변 컬렉션이다
    - 따라서 언제든 항상 같은 요소를 가진다

--- src/Beer.scala
+++ src/Beer.scala
@@ -3,5 +3,7 @@ object Beer {
     2 to 6 foreach { n => println(s"$n bottles of beer on the wall")}
 
     val authorsToAge = Map("Raoul" -> 23, "Mario" -> 40, "Alan" -> 53)
+    val authors = List("Raouol", "Mario", "Alan")
+    val numbers = Set(1, 2, 3, 4)
   }
 }

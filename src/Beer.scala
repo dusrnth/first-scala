3294e7940fb41f7ea9abcee69ba6443174bde0a9
kyupid <kyeonu1007@gmail.com>
2023-07-15 11:04:31 +0900

    Collection: Map
    - 스칼라는 타입을 모두 정적으로 체크한다.
    - Map 에 명시적으로 타입을 적어줄 수도 있지만 컴파일할때 타입이 정해진다

--- src/Beer.scala
+++ src/Beer.scala
@@ -1,5 +1,7 @@
 object Beer {
   def main(args: Array[String])= {
     2 to 6 foreach { n => println(s"$n bottles of beer on the wall")}
+
+    val authorsToAge = Map("Raoul" -> 23, "Mario" -> 40, "Alan" -> 53)
   }
 }
